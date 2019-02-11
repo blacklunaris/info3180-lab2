dc4293bdc0b4d9c4c087c9281bfaf4de4d3c47e4
Patrick Anglin <patrick.anglin02@mymona.uwi.edu>
2019-02-11 03:05:47 +0000

    my lab2 commit

--- app/views.py
+++ app/views.py
@@ -55,8 +55,6 @@ def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
 
-def format_date_joined(date):
-    
-    return date
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="8080")
